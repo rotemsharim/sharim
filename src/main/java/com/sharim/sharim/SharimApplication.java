3afcebdd38563eb69cd6d5ab86a28e957028fe7c
Rotem Shor <rotem@medisafe.com>
2018-01-22 07:22:06 +0200

    First commit

---
+++ src/main/java/com/sharim/sharim/SharimApplication.java
@@ -0,0 +1,12 @@
+package com.sharim.sharim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharimApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sharim/sharim/conf/SharimConfiguration.java
@@ -0,0 +1,7 @@
+package com.sharim.sharim.conf;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SharimConfiguration {
+}
