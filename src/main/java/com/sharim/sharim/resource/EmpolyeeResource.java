016a7d0ca9abd0e1a236268b5275886d7b70bdba
Rotem Shor <rotem@medisafe.com>
2018-01-23 10:52:35 +0200

    find employee

--- src/main/java/com/sharim/sharim/resource/EmpolyeeResource.java
+++ src/main/java/com/sharim/sharim/resource/EmpolyeeResource.java
@@ -5,6 +5,7 @@ import com.sharim.sharim.dto.EmployeeDto;
 import com.sharim.sharim.entities.Employee;
 import com.sharim.sharim.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/employees")
 public class EmpolyeeResource {
 
     @Autowired
@@ -21,11 +23,16 @@ public class EmpolyeeResource {
     @Autowired
     EmployeeToEmployeeDtoConverter employeeToEmployeeDtoConverter;
 
-    @RequestMapping("/employees")
+    @RequestMapping
     public @ResponseBody List<EmployeeDto> allEmployees() {
         return employeeRepository.findAll()
                 .stream().map(e -> employeeToEmployeeDtoConverter.convert(e))
                 .collect(Collectors.toList());
+    }
+
+    @RequestMapping("/{id}")
+    public @ResponseBody EmployeeDto findEmployee(@PathVariable("id") String id) {
+        return employeeToEmployeeDtoConverter.convert(employeeRepository.getOne(id));
     }
 
 }
